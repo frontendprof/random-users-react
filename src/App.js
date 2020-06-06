66ad4d4cad0acef8ea7420bd8446e135c8677b1a
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-06 13:10:37 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+import "./App.css"
+
+
+
+
+
+class App extends Component {
+  render(){
+
+    return (
+    <h1 className="app">Assalaamu aleykum</h1>
+    
+    )
+
+  }
+  
+}
+
+export default App;
