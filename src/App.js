9d31905f465c41bc05fe749ccfcc94ad765fdfa3
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-07 22:17:29 +0500

    Fetching avatar and misc data

--- src/App.js
+++ src/App.js
@@ -8,12 +8,55 @@ import "./App.css"
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      users:[],
+      pageNum:1
+    }
+  }
+
+  componentDidMount(){
+    const url=`https://reqres.in/api/users?page=${this.state.pageNum}`;
+
+    fetch(url)
+      .then(res=>res.json())
+      .then(res=>this.setState({users:res.data}))
+      
+
+  }
+
+
+
   render(){
+    const {users}=this.state;
 
     return (
-    <h1 className="app">Assalaamu aleykum</h1>
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Users' List</h1>
+        </div>
+
+        
+          
+        {users.map(user=>(
+
+          <div className="card" key={user.id}>
+          <img src={user.avatar} className="card-img-top" alt={`${user.email} avatar`} />
+          <div className="card-body">
+          <h5 className="card-title">{user.first_name} {user.last_name}</h5>
+          <p className="card-text">{user.email}</p>
+          
+          </div>
+          </div>
+
+        ))}  
+        
+        
+      </div>
     
-    )
+    );
 
   }
   
